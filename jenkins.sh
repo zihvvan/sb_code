4f1efd5989e74f653bb05f14f20990ddb8dcd243
root <pcmin929>
2023-02-24 01:25:59 +0000

    first commit

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get update -y
+apt-get install -y openjdk-11-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FCEF32E745F2C3D5
+sudo apt-get -y update
+sudo apt-get -y install jenkins
+sed -i s/HTTP_PORT=8080/HTTP_PORT=7777/g /etc/default/jenkins
+sed -i s/JENKINS_PORT=8080/JENKINS_PORT=7777/g /usr/lib/systemd/system/jenkins.service
+systemctl daemon-reload
+systemctl restart jenkins
+systemctl enable jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
